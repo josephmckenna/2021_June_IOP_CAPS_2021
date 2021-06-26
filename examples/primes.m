a48a261a799ddb417bc23211f01edc22f78a926b
Joseph McKenna <joseph.mckenna@cern.ch>
2021-06-26 17:36:30 +0200

    Add primes example written in Octave (MATLAB)

---
+++ examples/primes.m
@@ -0,0 +1,19 @@
+# In Octave a script cannot start with the keyword 'function'
+disp("Running")
+
+function retval = primes(n)
+    retval = true;
+    for i = 2:round(n/2)
+        if ( mod (n , i) == 0)
+            retval = false;
+            return
+        endif
+    endfor
+endfunction
+
+for i = [2:100]
+    if primes(i)
+        disp(i)
+    endif
+endfor
+        
