8b3d7081797e1db260de2859b92fb0d5c31a28fc
Joseph McKenna <joseph.mckenna@cern.ch>
2021-06-26 16:57:42 +0200

    Add C++ primes example

---
+++ examples/primes.cxx
@@ -0,0 +1,22 @@
+
+#include <iostream>
+
+bool IsPrime(int n)
+{
+    for ( int i = 2; i <= n / 2; ++i) {
+        if (n % i == 0) {
+           return false;
+        }
+    }
+ return true;
+}
+
+
+int main()
+{
+    for (int i = 2; i< 100; i++)
+    {
+        if (IsPrime(i))
+           std::cout <<i<<"\n";
+    }
+}
